2b7b6d02fc274a1af5cc1f5c78e0a970ec8c3668
marwan-at-work <marwan.sameer@gmail.com>
2018-03-27 17:11:06 -0400

    update

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"github.com/marwan-at-work/direct"
+	"github.com/marwan-at-work/direct-five"
+	"github.com/marwan-at-work/direct-four"
+	"github.com/marwan-at-work/direct-three"
+	"github.com/marwan-at-work/direct-two"
 )
 
 func main() {
 	direct.Hello()
+	directtwo.Hello()
+	directthree.Hello()
+	directfour.Hello()
+	directfive.Hello()
 }
